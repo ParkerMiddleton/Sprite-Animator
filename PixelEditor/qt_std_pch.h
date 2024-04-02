aca2826844cf9770bccbd13a6f0bec2e6b37fb66
garlikot <chesnokovegor12@gmail.com>
2024-04-01 21:28:39 -0600

    Cleaned up precompiled header includes.

--- PixelEditor/qt_std_pch.h
+++ PixelEditor/qt_std_pch.h
@@ -1,46 +1,6 @@
 #ifndef QT_STD_PCH_H
 #define QT_STD_PCH_H
 
-#include <QApplication>
-
-#include <QPixmap>
-#include <QMouseEvent>
-#include <QLabel>
-#include <QPaintDevice>
-#include <QObject>
-#include <QMainWindow>
-#include <QInputDialog>
-#include <QMessageBox>
-#include <QImageWriter>
-#include <QColorDialog>
-#include <QPalette>
-#include <QWidget>
-#include <QColor>
-#include <QList>
-#include <QFileInfo>
-#include <QDir>
-#include <QFile>
-#include <QPainter>
-#include <QDebug>
-#include <QSizePolicy>
-#include <QFileDialog>
-
-#include <QByteArray>
-
-#include <QJsonDocument>
-#include <QJsonObject>
-#include <QJsonArray>
-
 #include <QtWidgets>
-
-#include <QHBoxLayout>
-
-#include <QScrollArea>
-#include <QVector>
-
-#include <QLabel>
-#include <QLineEdit>
-#include <QDialogButtonBox>
-#include <QFormLayout>
 
 #endif // QT_STD_PCH_H
